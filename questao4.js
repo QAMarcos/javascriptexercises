5ff8f4bc70a14194fab89a1fcaaabbfa41434cde
Marcos Paulo <marcospereira.contact@gmail.com>
2021-09-19 17:38:46 -0300

    Terceiro commit

---
+++ questao4.js
@@ -0,0 +1,11 @@
+/* Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores */
+
+const apportion = function (a, b) {
+    result = a / b
+
+    return result
+
+}
+
+console.log (apportion(10,2))
