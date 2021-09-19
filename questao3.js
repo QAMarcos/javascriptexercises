ca38bd1dad1bc0a11a3eef27777ec2e8a4ecd95b
Marcos Paulo <marcospereira.contact@gmail.com>
2021-09-19 17:15:59 -0300

    segundo commit

--- questao3.js
+++ questao3.js
@@ -1,1 +1,10 @@
-git config --global user.name "Marcos"
+/* Crie uma função que recebe dois parâmetros, base e expoente, e 
+retorne a base elevada ao expoente. */
+
+const operation = function (param1, param2){
+    let result = Math.pow(param1, param2)
+
+    return result
+}
+
+console.log(operation(4,2))
